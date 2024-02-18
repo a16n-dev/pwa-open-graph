43bc8fe0610ad993f6ee67773467cf3c30e611ac
Alexander Nicholson <alexn400@gmail.com>
2024-02-18 15:53:01 -0500

    Add basic root layout

--- next-app/src/app/not-found.tsx
+++ next-app/src/app/not-found.tsx
@@ -1,4 +1,5 @@
-export async function GET() {
-  // return a permanent redirect to the SPA
-  return Response.redirect(process.env.NEXT_PUBLIC_SPA_URL || "", 301)
+import ClientSPARedirect from "@/components/ClientSPARedirect";
+
+export default function NotFound() {
+  return <ClientSPARedirect/>
 }
